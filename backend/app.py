fb0d43f9352879f546bc133ccb3e0fcb86db1270
Junwei <150350872+junnnwei@users.noreply.github.com>
2025-01-29 17:23:40 +0800

    Add: Keystroke Standard Deviation

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS  # Import CORS
+import math, csv, json
 app = Flask(__name__, static_folder="../static", template_folder="../templates")
 
 # Allow both localhost and 127.0.0.1 origins
 CORS(app, resources={r"/api/*": {"origins": ["http://127.0.0.1:5000", "http://localhost:5000"]}})
-
 
 # Login page
 @app.route('/')
@@ -21,12 +21,35 @@ def signup():
 def feedback():
     return render_template('feedback.html')
 
+# Backend metric processing
+def calculateKeystrokeSD(internval_data):
+    if len(internval_data) == 0:
+        return 0
+
+    mean = sum(internval_data) / len(internval_data)
+    variance = sum([(x - mean) ** 2 for x in internval_data]) / len(internval_data)
+    return math.sqrt(variance)
+
+def recalculateMetrics(data):
+    keystroke_intervals = data.get('keyPressIntervals', [])
+
+    std_dev = calculateKeystrokeSD(keystroke_intervals)
+    data['keystroke_sd'] = std_dev
+
 # API
 @app.route('/api/analysis-metrics', methods=['POST'])
 def analysis_metrics():
     try:
         data = request.get_json()
         print("Received metrics:", data)
+
+        recalculateMetrics(data)
+
+        print("\nCalculated metrics:", data)
+
+        # Note: ONLY include this line when attempting to collect data (TO BE REMOVED POST-DATA COLLECTION)
+        # append_to_csv(data)
+
         return jsonify({"status": "success", "message": "Metrics received!"}), 200
 
     except Exception as e:
