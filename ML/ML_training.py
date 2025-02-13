319ee2d02edfb7205607c781e9282716bebd62a1
jun <junwei_wong@outlook.com>
2025-02-13 19:37:46 +0800

    Add: synthetic training data for humans

--- ML/ML_training.py
+++ ML/ML_training.py
@@ -4,21 +4,41 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score
 
 # Load datasets
-human_data = pd.read_csv('./ML/datasets/human_training_dataset.csv')
-bot_data = pd.read_csv('./ML/datasets/bot_training_dataset.csv')
+human_data = pd.read_csv('../ML/datasets/human_training_dataset_synthetic.csv')
+bot_data = pd.read_csv('../ML/datasets/bot_training_dataset.csv')
 
 # Add labels: 0 for humans, 1 for bots
 human_data['label'] = 0
 bot_data['label'] = 1
 
+# print(human_data.dtypes)
+# print(bot_data.dtypes)
+
+# Check for invalid entries
+# print("Human Data")
+# print(human_data[['totalTimeSpentOnPage', 'averageTimePerField', 'mousespeed_sd', 'keystroke_sd']].describe())
+#
+# print("Bot Data")
+# print(bot_data[['totalTimeSpentOnPage', 'averageTimePerField', 'mousespeed_sd', 'keystroke_sd']].describe())
+
+# Clean up - remove 0.00 keystroke_sd for human - practically impossible
+# print(human_data[human_data['keystroke_sd'] == 0])
+human_data = human_data[(human_data['keystroke_sd'] != 0)]
+
+# Reset index after filtering
+human_data = human_data.reset_index(drop=True)
+# pd.set_option('display.float_format', '{:.2f}'.format)
+# print(human_data[['totalTimeSpentOnPage', 'averageTimePerField', 'mousespeed_sd', 'keystroke_sd']].describe())
+
 data = pd.concat([human_data, bot_data], ignore_index=True)
+# print(data.head())
 
 # Weights for feature engineering. Adjust if needed
 scaling_factors = {
-    'totalTimeSpentOnPage': 3.0,
-    'averageTimePerField': 3.0,
-    'mousespeed_sd': 1.5,
-    'keystroke_sd': 3.0
+    'totalTimeSpentOnPage': 10.0,
+    'averageTimePerField': 10.0,
+    'mousespeed_sd': 3.5,
+    'keystroke_sd': 15.0
 }
 
 for feature, factor in scaling_factors.items():
@@ -56,18 +76,15 @@ print(f"F1 Score: {f1:.2f}")
 print("\nClassification Report:\n", classification_report(y_val, y_pred))
 
 # Load test dataset
-test_data = pd.read_csv('./ML/datasets/test_dataset.csv')
+test_data = pd.read_csv('../ML/datasets/test_dataset.csv')
 
 # Add missing columns as zeros and drop extra columns
-for col in X.columns:
-    if col not in test_data.columns:
-        test_data[col] = 0
-test_data = test_data[X.columns]
+test_data = test_data.reindex(columns=X.columns, fill_value=0)
 
 # Predict on test data
 test_predictions_proba = rf_model.predict_proba(test_data)[:, 1]  # Probability of being a bot
 
 # Save prediction results
 output = pd.DataFrame({'Bot_Probability': test_predictions_proba})
-output.to_csv('./ML/datasets/ML_predictions.csv', index=False)
+output.to_csv('../ML/datasets/ML_predictions.csv', index=False)
 print("Predictions saved to ML_predictions.csv.")
